9aede3dbbf5d6fd3f75f7b63ffb4486c05cd9b09
Lorsuz <91427731+Lorsuz@users.noreply.github.com>
2023-10-02 19:32:34 -0300

    falta o outlet

---
+++ backend/app.js
@@ -0,0 +1,50 @@
+// server.js
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+const app = express();
+const port = 3001;
+ 
+// Replace this with your own secret key for JWT
+const secretKey = 'your-secret-key';
+
+// Sample user data (in-memory database)
+const users = [ 
+  { id: 1, username: 'user1', password: 'password1' },
+  { id: 2, username: 'user2', password: 'password2' },
+];
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// Register a new user
+app.post('/api/register', (req, res) => {
+  const { username, password } = req.body;
+  // Add validation logic here (e.g., check if username is unique)
+  // In a real app, store user data securely (e.g., in a database)
+  users.push({ id: users.length + 1, username, password });
+	users.forEach(element => {
+		console.log(element);
+	});
+  res.json({ message: 'Registration successful' });
+});
+
+// Login and get a JWT token 
+app.post('/api/login', (req, res) => { 
+  const { username, password } = req.body;
+  // Add validation logic here (e.g., check credentials against stored data)
+  const user = users.find((u) => u.username === username && u.password === password);
+  if (!user) {
+    res.status(401).json({ message: 'Invalid credentials' });
+  } else {
+    const token = jwt.sign({ userId: user.id }, secretKey);
+		console.log(token);
+    res.json({ message: 'Login successful', token });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,148 @@
+// src/App.js
+
+import React, { useState } from 'react';
+import { Route, Routes, Link, Navigate, Outlet, useNavigate } from 'react-router-dom';
+
+function App () {
+	const [ token, setToken ] = useState( '' );
+	const navigate = useNavigate();
+
+
+	// Replace with your backend API URL
+	const apiUrl = 'http://localhost:3001/api';
+
+	const handleRegister = async ( username, password ) => {
+		try {
+			const response = await fetch( `${ apiUrl }/register`, {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify( { username, password } ),
+			} );
+			const data = await response.json();
+			console.log( data.message );
+		} catch ( error ) {
+			console.error( 'Error:', error );
+		}
+	};
+
+	const handleLogin = async ( username, password ) => {
+		try {
+			const response = await fetch( `${ apiUrl }/login`, {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify( { username, password } ),
+			} );
+			const data = await response.json();
+			if ( response.ok ) {
+				console.log( data.token );
+				setToken( data.token );
+				navigate('/private/application');
+			} else {
+				console.log( data.message );
+			}
+		} catch ( error ) {
+			console.error( 'Error:', error );
+		}
+	};
+
+	return (
+			<div className="App">
+				<h1>Authentication Example</h1>
+				<nav>
+					<ul>
+						<li>
+							<Link to="/login">Login</Link>
+						</li>
+						<li>
+							<Link to="/register">Register</Link>
+						</li>
+					</ul>
+				</nav>
+				<Routes>
+					<Route path="/login" element={ <Login onLogin={ handleLogin } /> } />
+					<Route path="/register" element={ <Register onRegister={ handleRegister } /> } />
+					<Route path="/private" element={ <PrivateRoute token={ token } /> } >
+						<Route path='application' element={<Application/>}/>
+					</Route>
+					<Route index element={ <Navigate to="/login" /> } />
+				</Routes>
+			</div>
+	);
+}
+
+function Login ( { onLogin } ) {
+	const [ username, setUsername ] = useState( '' );
+	const [ password, setPassword ] = useState( '' );
+
+	const handleSubmit = ( e ) => {
+		e.preventDefault();
+		onLogin( username, password );
+	};
+
+	return (
+		<div>
+			<h2>Login</h2>
+			<form onSubmit={ handleSubmit }>
+				<input
+					type="text"
+					placeholder="Username"
+					value={ username }
+					onChange={ ( e ) => setUsername( e.target.value ) }
+				/>
+				<input
+					type="password"
+					placeholder="Password"
+					value={ password }
+					onChange={ ( e ) => setPassword( e.target.value ) }
+				/>
+				<button type="submit">Login</button>
+			</form>
+		</div>
+	);
+}
+
+function Register ( { onRegister } ) {
+	const [ username, setUsername ] = useState( '' );
+	const [ password, setPassword ] = useState( '' );
+
+	const handleSubmit = ( e ) => {
+		e.preventDefault();
+		onRegister( username, password );
+	};
+
+	return (
+		<div>
+			<h2>Register</h2>
+			<form onSubmit={ handleSubmit }>
+				<input
+					type="text"
+					placeholder="Username"
+					value={ username }
+					onChange={ ( e ) => setUsername( e.target.value ) }
+				/>
+				<input
+					type="password"
+					placeholder="Password"
+					value={ password }
+					onChange={ ( e ) => setPassword( e.target.value ) }
+				/>
+				<button type="submit">Register</button>
+			</form>
+		</div>
+	);
+}
+
+function PrivateRoute ( { token } ) {
+	return token ? <Outlet /> : <Navigate to="/login" />;
+}
+
+function Application() {
+	return (
+		<div>
+			<h1>Application</h1>
+			<p>This is a private route.</p>
+		</div>
+	);
+}
+
+export default App;
